bc3535b7c35cb6cbe6d463f7234bd4a474813161
gig4ntic <118375282+gig4ntic@users.noreply.github.com>
2025-02-15 15:46:44 +0100

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -2,6 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+response = requests.get(url, headers=headers)
+
 def get_free_stalls(url):
     try:
         response = requests.get(url)
